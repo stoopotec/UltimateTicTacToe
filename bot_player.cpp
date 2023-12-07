ef48b33e943ca91801b36ed9c919541397884246
stoopotec <stoopotec@tuta.io>
2023-12-08 00:31:13 +0300

    добавлена функция rate "первого уровня" высчитывает примерный шанс на победу первого игрока

--- bot_player.cpp
+++ bot_player.cpp
@@ -4,7 +4,26 @@
 BotPlayer::BotPlayer() { }
 
 
+/// @brief рассичтывает примерный "шанс" победить первому (крестик) игроку
+/// @param ground 
+/// @return число от 0.0 до 1.0 
+double rate(playground& ground)
+{
+    // x - o, тоесть количество поставленных и доступных крестиков минус ноликов (крестик - первый игрок, нолик- второй игрок)
+    static int xmo, x, y, c;
+    xmo = 0;
+
+    for (x = 0; x < 9; x++) 
+        for (y = 0; y < 9; y++)
+        {
+            c = ground.get_cell_on_pos(x, y);
+            if (c == CELL_PL1) xmo++;
+            if (c == CELL_PL2) xmo--;
+        }
+
+    return ((double)xmo / 81.0) * 0.5 + 0.5;
+}
+
 void BotPlayer::next_move(int& x, int& y, playground& playground) {
-    x = 0;
-    y = 0;
+    
 }
