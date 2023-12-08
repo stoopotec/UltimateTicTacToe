3ba514be2181f2e045ad1785cf2e18dd1ecf3394
stoopotec <stoopotec@tuta.io>
2023-12-08 20:26:50 +0300

    убрал тесты (перенесу в tests)

--- main.cpp
+++ main.cpp
@@ -2,38 +2,6 @@
 #include "playground.hpp"
 #include "player.hpp"
 
-void gp(playground& g)
-{
-    printf("board view:\n");
-    for (int y = 0; y < 9; y++)
-    {
-        for (int x = 0; x < 9; x++)
-        {
-            unsigned char c = g.get_cell_on_pos(x, y);
-            putchar(cell_to_char(c));
-        }
-        putchar('\n');
-    }
-    putchar('\n');
-    printf("bits view:\n");
-    printf("cell to char  |  byte\n");
-    for (int i = 0; i < PLAYGROUND_BYTES; i++) {
-        unsigned char gb = *(g.get_ground() + i);
-
-        for (int b = 0; b < sizeof(unsigned char) * 8; b += 2) {
-            putchar(cell_to_char((gb >> b) & (unsigned char)0b11));
-            putchar(' ');
-        }
-        printf("      |  ");
-        for (int b = 0; b < sizeof(unsigned char) * 8; b += 2) {
-            putchar(((gb >>  b   ) & (unsigned char)0b1) ? '1' : '0');
-            putchar(((gb >> (b+1)) & (unsigned char)0b1) ? '1' : '0');
-            putchar(' ');
-        }
-        putchar('\n');
-    }
-}
-
 int main(int argc, char** argv)
 {
     // ЭТО ПРИМЕР!!!! желательно делать так, но если что-то придумаешь прикольное, то можешь мне предложить и делать
@@ -47,16 +15,5 @@ int main(int argc, char** argv)
     players[1] = &p2;
 
     playground ground = playground();
-
-    printf("press any key to see a magic!\n");
-    for (unsigned char fill = 0b01; fill < 0b100; fill--)
-        for (int y = 0; y < 9; y++)
-            for (int x = 0; x < 9; x++)
-            {
-                std::cin.get();
-                ground.set_cell_on_pos(x, y, fill);
-                system("clear");
-                gp(ground);
-            }
     
 }
